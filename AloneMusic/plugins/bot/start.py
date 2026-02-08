3c67f08a97c0eae3f00646384ac830db040941d9
𝐇𝐀𝐂𝐊𝐄𝐑 <ayushprincekmr03@gmail.com>
2026-02-08 20:42:35 +0530

    Update start.py

--- AloneMusic/plugins/bot/start.py
+++ AloneMusic/plugins/bot/start.py
@@ -1,7 +1,15 @@
+#
+# Copyright (C) 2021-2022 by TheAloneteam@Github, < https://github.com/TheAloneTeam >.
+# This file is part of < https://github.com/TheAloneTeam/AloneMusic > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TheAloneTeam/AloneMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+
 import random
 import time
-import asyncio
-from youtubesearchpython.__future__ import VideosSearch
+
+from py_yt import VideosSearch
 from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
@@ -15,7 +23,7 @@ from AloneMusic.utils.database import (add_served_chat, add_served_user,
                                        is_banned_user, is_on_off)
 from AloneMusic.utils.decorators.language import LanguageStart
 from AloneMusic.utils.formatters import get_readable_time
-from AloneMusic.utils.inline import first_page, private_panel, start_panel
+from AloneMusic.utils.inline import help_pannel, private_panel, start_panel
 from config import BANNED_USERS
 from strings import get_string
 
@@ -26,36 +34,18 @@ EFFECT_ID = [
     5159385139981059251,
 ]
 
-START_IMAGES = [
-    "https://files.catbox.moe/2bia7i.jpg",
-    "https://files.catbox.moe/vr5r9r.jpg",
-    "https://files.catbox.moe/pltowg.jpg",
-    "https://files.catbox.moe/cudd9u.jpg"
-]
-
-async def change_img():
-    global START_IMAGES
-    while True:
-        await asyncio.sleep(5)
-        random.shuffle(START_IMAGES)
-
-async def start_task():
-    asyncio.create_task(change_img())
 
 @app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
 async def start_pm(client, message: Message, _):
     await add_served_user(message.from_user.id)
     await message.react("🍓")
-    
-    random_image = random.choice(START_IMAGES)
-    
     if len(message.text.split()) > 1:
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
-            keyboard = first_page(_)
+            keyboard = help_pannel(_)
             return await message.reply_photo(
-                photo=random_image,
+                photo=config.START_IMG_URL,
                 has_spoiler=True,
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
@@ -109,7 +99,7 @@ async def start_pm(client, message: Message, _):
     else:
         out = private_panel(_)
         await message.reply_photo(
-            photo=random_image,
+            photo=config.START_IMG_URL,
             has_spoiler=True,
             message_effect_id=random.choice(EFFECT_ID),
             caption=_["start_2"].format(message.from_user.mention, app.mention),
@@ -121,19 +111,20 @@ async def start_pm(client, message: Message, _):
                 text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
             )
 
+
 @app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
 @LanguageStart
 async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
-    random_image = random.choice(START_IMAGES)
     await message.reply_photo(
-        photo=random_image,
+        photo=config.START_IMG_URL,
         has_spoiler=True,
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
     )
     return await add_served_chat(message.chat.id)
+
 
 @app.on_message(filters.new_chat_members, group=-1)
 async def welcome(client, message: Message):
@@ -162,9 +153,8 @@ async def welcome(client, message: Message):
                     return await app.leave_chat(message.chat.id)
 
                 out = start_panel(_)
-                random_image = random.choice(START_IMAGES)
                 await message.reply_photo(
-                    photo=random_image,
+                    photo=config.START_IMG_URL,
                     has_spoiler=True,
                     caption=_["start_3"].format(
                         message.from_user.first_name,

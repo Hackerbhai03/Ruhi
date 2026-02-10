a37af5abf2810c2f2c8799b84eda226fb229cf83
𝐇𝐀𝐂𝐊𝐄𝐑 <ayushprincekmr03@gmail.com>
2026-02-10 20:59:21 +0530

    Update Gpt.py

--- AloneMusic/plugins/tools/Gpt.py
+++ AloneMusic/plugins/tools/Gpt.py
@@ -1,6 +1,6 @@
 import time
 import requests
-from RiyaMusic import app
+from AloneMusic import app
 from config import BOT_USERNAME
 from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters

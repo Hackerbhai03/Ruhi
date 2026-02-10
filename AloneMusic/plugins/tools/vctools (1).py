2e5bdd9a09d05173de11d4a681842ec7296de722
𝐇𝐀𝐂𝐊𝐄𝐑 <ayushprincekmr03@gmail.com>
2026-02-10 21:13:49 +0530

    Update vctools (1).py

--- AloneMusic/plugins/tools/vctools (1).py
+++ AloneMusic/plugins/tools/vctools (1).py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from RiyaMusic import app
+from AloneMusic import app
 from config import OWNER_ID
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.enums import ChatType, ChatMemberStatus

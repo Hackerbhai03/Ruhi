5e02eaa21e0972529c329385ec906054bfd2b1a4
𝐇𝐀𝐂𝐊𝐄𝐑 <ayushprincekmr03@gmail.com>
2026-02-19 17:54:39 +0530

    Clean up copyright and fix import statement
    Removed copyright information and updated import path.

--- AloneMusic/plugins/tools/babusona.py
+++ AloneMusic/plugins/tools/babusona.py
@@ -1,29 +1,7 @@
-# Copyright (c) 2025 Nand Yaduwanshi <NoxxOP>
-# Location: Supaul, Bihar
-#
-# All rights reserved.
-#
-# This code is the intellectual property of Nand Yaduwanshi.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: badboy809075@gmail.com
-
-
 import random
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from ShrutiMusic import app
+from AloneMusic import app
 import asyncio
 
 GALI = [
@@ -207,13 +185,3 @@ Note: This command is restricted in groups to maintain decorum.
 """
 
 
-# ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
-
-# ===========================================
-# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
-# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
-# 📢 Telegram Channel : https://t.me/ShrutiBots
-# ===========================================
-
-
-# ❤️ Love From ShrutiBots 

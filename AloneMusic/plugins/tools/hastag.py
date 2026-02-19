92b8336d44b692c54d31125bd891eb7f26ebee5e
𝐇𝐀𝐂𝐊𝐄𝐑 <ayushprincekmr03@gmail.com>
2026-02-19 19:21:39 +0530

    Update hastag.py

--- AloneMusic/plugins/tools/hastag.py
+++ AloneMusic/plugins/tools/hastag.py
@@ -1,19 +1,6 @@
-# -----------------------------------------------
-# 🔸 StrangerMusic Project
-# 🔹 Developed & Maintained by: Shashank Shukla (https://github.com/itzshukla)
-# 📅 Copyright © 2022 – All Rights Reserved
-#
-# 📖 License:
-# This source code is open for educational and non-commercial use ONLY.
-# You are required to retain this credit in all copies or substantial portions of this file.
-# Commercial use, redistribution, or removal of this notice is strictly prohibited
-# without prior written permission from the author.
-#
-# ❤️ Made with dedication and love by ItzShukla
-# -----------------------------------------------
 import requests
 from bs4 import BeautifulSoup as  BSP
-from SHUKLAMUSIC import app as SHUKLA
+from AloneMusic import app as SHUKLA
 from pyrogram import filters
 url = "https://all-hashtag.com/library/contents/ajax_generator.php"
 

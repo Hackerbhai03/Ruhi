11c4da72e983a954e992b9245e95e947e2d711bf
𝐇𝐀𝐂𝐊𝐄𝐑 <ayushprincekmr03@gmail.com>
2026-02-20 01:15:22 +0530

    Update error.py

--- AloneMusic/utils/error.py
+++ AloneMusic/utils/error.py
@@ -1,11 +1,3 @@
-#
-# Copyright (C) 2021-2022 by TheAloneteam@Github, < https://github.com/TheAloneTeam >.
-# This file is part of < https://github.com/TheAloneTeam/AloneMusic > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TheAloneTeam/AloneMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-
 import os
 import sys
 import traceback
@@ -13,7 +5,11 @@ from datetime import datetime
 from functools import wraps
 
 import aiofiles
-from pyrogram.error.exceptions.forbidden_403 import ChatWriteForbidden
+
+try:
+    from pyrogram.errors import ChatWriteForbidden
+except ImportError:
+    from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
 from AloneMusic import app
 from AloneMusic.utils.exceptions import is_ignored_error

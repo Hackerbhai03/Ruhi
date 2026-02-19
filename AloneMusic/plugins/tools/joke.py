60bb7520cbb5cacaf5e7b560cf1840ef054e9248
𝐇𝐀𝐂𝐊𝐄𝐑 <ayushprincekmr03@gmail.com>
2026-02-19 19:24:16 +0530

    Update joke.py

--- AloneMusic/plugins/tools/joke.py
+++ AloneMusic/plugins/tools/joke.py
@@ -1,18 +1,5 @@
-# -----------------------------------------------
-# 🔸 StrangerMusic Project
-# 🔹 Developed & Maintained by: Shashank Shukla (https://github.com/itzshukla)
-# 📅 Copyright © 2022 – All Rights Reserved
-#
-# 📖 License:
-# This source code is open for educational and non-commercial use ONLY.
-# You are required to retain this credit in all copies or substantial portions of this file.
-# Commercial use, redistribution, or removal of this notice is strictly prohibited
-# without prior written permission from the author.
-#
-# ❤️ Made with dedication and love by ItzShukla
-# -----------------------------------------------
 import requests
-from SHUKLAMUSIC import app
+from AloneMusic import app
 from pyrogram import Client, filters
 
 JOKE_API_ENDPOINT = 'https://hindi-jokes-api.onrender.com/jokes?api_key=1a6d440e3f5971eecebceee818c2'

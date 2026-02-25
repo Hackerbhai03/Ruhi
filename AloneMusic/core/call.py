f04d9db2f0c3efdfbff40637077d17d0e307caa6
𝐇𝐀𝐂𝐊𝐄𝐑 <ayushprincekmr03@gmail.com>
2026-02-25 06:36:19 +0530

    Update call.py

--- AloneMusic/core/call.py
+++ AloneMusic/core/call.py
@@ -29,7 +29,7 @@ from AloneMusic.utils.formatters import (check_duration, seconds_to_min,
                                          speed_converter)
 from AloneMusic.utils.inline.play import stream_markup
 from AloneMusic.utils.stream.autoclear import auto_clean
-from AloneMusic.utils.thumbnails import gen_thumb
+from AloneMusic.utils.thumbnails import get_thumb
 from strings import get_string
 
 

5d20a77a51dc8d22bb1f9dedc1635a24c1cc148c
𝐇𝐀𝐂𝐊𝐄𝐑 <ayushprincekmr03@gmail.com>
2026-03-05 14:38:44 +0530

    Update thumbnails.py

--- AloneMusic/utils/thumbnails.py
+++ AloneMusic/utils/thumbnails.py
@@ -5,7 +5,7 @@ import aiofiles
 from AloneMusic import app
 from config import YOUTUBE_IMG_URL
 from PIL import Image, ImageDraw, ImageFont, ImageFilter, ImageEnhance
-from py_yt import VideosSearch
+from youtubesearchpython.__future__ import VideosSearch
 
 def clear(text):
     return re.sub("\s+", " ", text).strip()
